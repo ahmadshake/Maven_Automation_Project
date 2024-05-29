3d67d93811077f45ff85679bd37e7a5a0a30610e
ahmadshake <ahmadshake@outlook.com>
2024-05-28 22:46:12 -0400

    git hello world

---
+++ src/test/java/Day16_05_28_2024/Github_Test.java
@@ -0,0 +1,8 @@
+package Day16_05_28_2024;
+
+public class Github_Test {
+        public static void main(String[] args) {
+            //print hello world to the console
+            System.out.println("Hello World");
+        }//end of main method
+    }//end of class
